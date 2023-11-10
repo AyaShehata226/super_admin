20591c6836c0bbb35454c379bc9aa320a7363f70
AyaShehata226 <ayashegata@gmail.com>
2023-11-10 09:58:21 +0200

    super-admin

---
+++ super-admin/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidenavComponent } from './Components/sidenav/sidenav.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidenavComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
